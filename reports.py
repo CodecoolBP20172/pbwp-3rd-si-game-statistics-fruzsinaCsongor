d32c22b213c81eec5b3893967667cb7aab8e991b
Fruzsina Csongor <csongor.fruzsina@gmail.com>
2017-06-26 01:29:34 +0200

    'sort_abc' and 'get_genres' functions created

--- reports.py
+++ reports.py
@@ -1,3 +1,4 @@
+
 def count_games(file_name):
     with open(file_name) as inputfile:
         return sum(1 for _ in inputfile)
@@ -20,12 +21,32 @@ def decide(file_name, year):
 #
 
 
-# def sort_abc(file_name):
-#
+def sort_abc(file_name):
+    stats = {}
+    with open(file_name) as inputfile:
+        for line in inputfile:
+            title, copies, release, genre, publisher = (
+                item.strip() for item in line.split('\t', 4))
+            stats[title] = dict(zip(("title", "copies", "release", "genre", "publisher"),
+                                (title, copies, release, genre, publisher)))
+    abc_list = []
+    for game in sorted(stats.keys()):
+        abc_list.append(game)
+    return abc_list
 
 
-# def get_genres(file_name):
-#
+def get_genres(file_name):
+    stats = {}
+    with open(file_name) as inputfile:
+        for line in inputfile:
+            title, copies, release, genre, publisher = (
+                item.strip() for item in line.split('\t', 4))
+            stats[title] = dict(zip(("title", "copies", "release", "genre", "publisher"),
+                                (title, copies, release, genre, publisher)))
+    genres = set()
+    for game in stats.values():
+        genres.add(game["genre"])
+    return sorted(list(genres))
 
 
 # def when_was_top_sold_fps(file_name):
