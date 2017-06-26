af11090d7c0435afe97fdc5f42950c74c0af8616
Fruzsina Csongor <csongor.fruzsina@gmail.com>
2017-06-26 12:28:49 +0200

    'get_latest' and 'count_by_genre' functions created

--- reports.py
+++ reports.py
@@ -1,4 +1,15 @@
 
+from collections import OrderedDict
+
+stats = {}
+with open("game_stat.txt") as inputfile:
+    for line in inputfile:
+        title, copies, release, gen, publ = (
+            item.strip() for item in line.split('\t', 4))
+        stats[title] = dict(zip(("title", "copies", "release", "genre", "publisher"),
+                            (title, copies, release, gen, publ)))
+
+
 def count_games(file_name):
     with open(file_name) as inputfile:
         return sum(1 for _ in inputfile)
@@ -9,12 +20,21 @@ def decide(file_name, year):
         return any(year in line for line in inputfile)
 
 
-# def get_latest(file_name):
-#
+def get_latest(file_name):
+    release_dict = {}
+    for game in stats.values():
+        release_dict[game["title"]] = game["release"]
+    release_dict = OrderedDict(sorted(release_dict.items(), key=lambda t: t[1]))
+    return list(release_dict.items())[-1][0]
 
 
-# def count_by_genre(file_name, genre):
-#
+def count_by_genre(file_name, genre):
+    genre_list = []
+    for game in stats:
+        for value in stats[game].values():
+            if value == genre:
+                genre_list.append(game)
+    return len(genre_list)
 
 
 # def get_line_number_by_title(file_name, title):
@@ -22,13 +42,6 @@ def decide(file_name, year):
 
 
 def sort_abc(file_name):
-    stats = {}
-    with open(file_name) as inputfile:
-        for line in inputfile:
-            title, copies, release, genre, publisher = (
-                item.strip() for item in line.split('\t', 4))
-            stats[title] = dict(zip(("title", "copies", "release", "genre", "publisher"),
-                                (title, copies, release, genre, publisher)))
     abc_list = []
     for game in sorted(stats.keys()):
         abc_list.append(game)
@@ -36,13 +49,6 @@ def sort_abc(file_name):
 
 
 def get_genres(file_name):
-    stats = {}
-    with open(file_name) as inputfile:
-        for line in inputfile:
-            title, copies, release, genre, publisher = (
-                item.strip() for item in line.split('\t', 4))
-            stats[title] = dict(zip(("title", "copies", "release", "genre", "publisher"),
-                                (title, copies, release, genre, publisher)))
     genres = set()
     for game in stats.values():
         genres.add(game["genre"])
