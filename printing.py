dc35c0a5f834e16624bb350c53549faf31ad65bf
Fruzsina Csongor <csongor.fruzsina@gmail.com>
2017-06-27 10:45:09 +0200

    imported the functions from reports.py, created a function that prints the answers

--- printing.py
+++ printing.py
@@ -1,2 +1,13 @@
+from reports import count_games, decide, get_latest, count_by_genre, sort_abc, get_genres
 
-# Printing functions
+
+def print_func(file_name, year, genre):
+    print(count_games(file_name), "\n")
+    print(decide(file_name, year), "\n")
+    print(get_latest(file_name), "\n")
+    print(count_by_genre(file_name, genre), "\n")
+    print(sort_abc(file_name), "\n")
+    print(get_genres(file_name), "\n")
+
+
+print_func("game_stat.txt", "2000", "First-person shooter")
