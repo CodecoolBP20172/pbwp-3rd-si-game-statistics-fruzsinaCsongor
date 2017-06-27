fb3998ca90e8e3e4d17e62b323f691e81911a8a0
Fruzsina Csongor <csongor.fruzsina@gmail.com>
2017-06-27 22:57:44 +0200

    imported and added printing function for 'get_line_number_by_title' function

--- printing.py
+++ printing.py
@@ -1,13 +1,14 @@
-from reports import count_games, decide, get_latest, count_by_genre, sort_abc, get_genres
+from reports import count_games, decide, get_latest, count_by_genre, get_line_number_by_title, sort_abc, get_genres
 
 
-def print_func(file_name, year, genre):
+def print_the_answers(file_name, year, genre, title):
     print(count_games(file_name), "\n")
     print(decide(file_name, year), "\n")
     print(get_latest(file_name), "\n")
     print(count_by_genre(file_name, genre), "\n")
+    print(get_line_number_by_title(file_name, title), "\n")
     print(sort_abc(file_name), "\n")
     print(get_genres(file_name), "\n")
 
 
-print_func("game_stat.txt", "2000", "First-person shooter")
+print_the_answers("game_stat.txt", "2000", "First-person shooter", "Crysis")
