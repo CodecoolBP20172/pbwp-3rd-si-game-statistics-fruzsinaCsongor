bd1576cf5058459a8e3fb04dd09fd83a532f08bf
Fruzsina Csongor <csongor.fruzsina@gmail.com>
2017-06-28 00:04:22 +0200

    module is ugly but working with 7 report functions

--- export.py
+++ export.py
@@ -1,21 +1,15 @@
 from reports import count_games, decide, get_latest, count_by_genre, get_line_number_by_title, sort_abc, get_genres
 
 
-def export_to_txt(file_name, year, genre, title, exp_file="exported_answers.txt"):
+def export_to_txt(file_name, year, genre, title, exp_file="answers.txt"):
     with open(exp_file, "w") as text_file:
-        text_file.write(str(count_games(file_name)))
-        text_file.write("\n")
-        text_file.write(str(decide(file_name, year)))
-        text_file.write("\n")
-        text_file.write(str(get_latest(file_name)))
-        text_file.write("\n")
-        text_file.write(str(count_by_genre(file_name, genre)))
-        text_file.write("\n")
-        text_file.write(str(get_line_number_by_title(file_name, title)))
-        text_file.write("\n")
-        text_file.write(str(sort_abc(file_name)))
-        text_file.write("\n")
-        text_file.write(str(get_genres(file_name)))
+        text_file.write("{}\n{}\n{}\n{}\n{}\n{}\n{}\n".format(str(count_games(file_name)),
+                                                              str(decide(file_name, year)),
+                                                              str(get_latest(file_name)),
+                                                              str(count_by_genre(file_name, genre)),
+                                                              str(get_line_number_by_title(file_name, title)),
+                                                              str(sort_abc(file_name)),
+                                                              str(get_genres(file_name))))
 
 
-export_to_txt("game_stat.txt", "2000", "RPG", "Minecraft", exp_file="exported_answers.txt")
+export_to_txt("game_stat.txt", "2000", "RPG", "Minecraft", exp_file="answers.txt")
