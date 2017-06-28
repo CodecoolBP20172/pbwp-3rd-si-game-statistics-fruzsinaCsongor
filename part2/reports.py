38b644b171ea7152c9c69c43be43200b67c2d1f3
Fruzsina Csongor <csongor.fruzsina@gmail.com>
2017-06-28 21:58:59 +0200

    'sum_sold' and 'get_selling_avg functions created'

--- part2/reports.py
+++ part2/reports.py
@@ -1,2 +1,45 @@
+from statistics import mean
 
-# Report functions
+stats = {}
+with open("game_stat.txt") as inputfile:
+    for line in inputfile:
+        title, copies, release, gen, publ = (
+            item.strip() for item in line.split('\t', 4))
+        stats[title] = dict(zip(("title", "copies", "release", "genre", "publisher"),
+                            (title, copies, release, gen, publ)))
+
+
+# def get_most_played(file_name):
+
+
+def sum_sold(file_name):
+    copies_list = []
+    for val1 in stats.values():
+        for key2, val2 in val1.items():
+            if key2 == "copies":
+                copies_list.append(float(val2))
+    return round(sum(copies_list), 2)
+
+
+def get_selling_avg(file_name):
+    copies_list = []
+    for val1 in stats.values():
+        for key2, val2 in val1.items():
+            if key2 == "copies":
+                copies_list.append(float(val2))
+    return mean(copies_list)
+
+
+# def count_longest_title(file_name):
+
+
+# def get_date_avg(file_name):
+
+
+# def get_game(file_name, title):
+
+
+# def count_grouped_by_genre(file_name):
+
+
+# def get_date_ordered(file_name):
