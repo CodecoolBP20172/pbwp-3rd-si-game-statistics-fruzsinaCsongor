6a29042acc7c5beedd123bd71d3e180ea5c21c8c
Fruzsina Csongor <csongor.fruzsina@gmail.com>
2017-06-28 12:16:25 +0200

    added 'when_was_top_sold_fps' function

--- printing.py
+++ printing.py
@@ -1,14 +1,15 @@
-from reports import count_games, decide, get_latest, count_by_genre, get_line_number_by_title, sort_abc, get_genres
+import reports
 
 
 def print_the_answers(file_name, year, genre, title):
-    print(count_games(file_name), "\n")
-    print(decide(file_name, year), "\n")
-    print(get_latest(file_name), "\n")
-    print(count_by_genre(file_name, genre), "\n")
-    print(get_line_number_by_title(file_name, title), "\n")
-    print(sort_abc(file_name), "\n")
-    print(get_genres(file_name), "\n")
+    print(reports.count_games(file_name))
+    print(reports.decide(file_name, year))
+    print(reports.get_latest(file_name))
+    print(reports.count_by_genre(file_name, genre))
+    print(reports.get_line_number_by_title(file_name, title))
+    print(reports.sort_abc(file_name))
+    print(reports.get_genres(file_name))
+    print(reports.when_was_top_sold_fps(file_name))
 
 
 print_the_answers("game_stat.txt", "2000", "First-person shooter", "Crysis")
