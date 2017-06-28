2079d482308be3dca3f6ca5c53923fdc4cf98353
Fruzsina Csongor <csongor.fruzsina@gmail.com>
2017-06-28 12:17:14 +0200

    added 'when_was_top_sold_fps' function

--- export.py
+++ export.py
@@ -1,15 +1,17 @@
-from reports import count_games, decide, get_latest, count_by_genre, get_line_number_by_title, sort_abc, get_genres
+import reports
 
 
 def export_to_txt(file_name, year, genre, title, exp_file="answers.txt"):
     with open(exp_file, "w") as text_file:
-        text_file.write("{}\n{}\n{}\n{}\n{}\n{}\n{}\n".format(str(count_games(file_name)),
-                                                              str(decide(file_name, year)),
-                                                              str(get_latest(file_name)),
-                                                              str(count_by_genre(file_name, genre)),
-                                                              str(get_line_number_by_title(file_name, title)),
-                                                              str(sort_abc(file_name)),
-                                                              str(get_genres(file_name))))
+        text_file.write("{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}".format(
+            str(reports.count_games(file_name)),
+            str(reports.decide(file_name, year)),
+            str(reports.get_latest(file_name)),
+            str(reports.count_by_genre(file_name, genre)),
+            str(reports.get_line_number_by_title(file_name, title)),
+            str(reports.sort_abc(file_name)),
+            str(reports.get_genres(file_name)),
+            str(reports.when_was_top_sold_fps(file_name))))
 
 
-export_to_txt("game_stat.txt", "2000", "RPG", "Minecraft", exp_file="answers.txt")
+export_to_txt("game_stat.txt", "2000", "First-person shooter", "Crysis", exp_file="answers.txt")
