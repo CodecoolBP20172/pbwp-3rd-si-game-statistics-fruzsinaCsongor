171886b1dafd70803288b4857572ebaaa9555e0d
Fruzsina Csongor <csongor.fruzsina@gmail.com>
2017-06-29 22:45:43 +0200

    added the exporting function that calls the functions of the reports module and exports the results to 'answers.txt'

--- part2/export.py
+++ part2/export.py
@@ -1,2 +1,14 @@
+import reports
 
-# Export functions
+
+def export_to_txt(file_name, exp_file="answers.txt"):
+    with open(exp_file, "w") as text_file:
+        text_file.write("{}\n{}\n{}\n{}\n{}".format(
+            str(reports.get_most_played(file_name)),
+            str(reports.sum_sold(file_name)),
+            str(reports.get_selling_avg(file_name)),
+            str(reports.count_longest_title(file_name)),
+            str(reports.get_date_avg(file_name))))
+
+
+export_to_txt("game_stat.txt", exp_file="answers.txt")
