0211598ce9a260d34ebfe7bdf9b9fed2fa585b56
Fruzsina Csongor <csongor.fruzsina@gmail.com>
2017-06-29 10:31:05 +0200

    'get_most_played' and 'count_longest_title' added

--- part2/reports.py
+++ part2/reports.py
@@ -1,4 +1,5 @@
 from statistics import mean
+from collections import OrderedDict
 
 stats = {}
 with open("game_stat.txt") as inputfile:
@@ -9,7 +10,13 @@ with open("game_stat.txt") as inputfile:
                             (title, copies, release, gen, publ)))
 
 
-# def get_most_played(file_name):
+def get_most_played(file_name):
+    copies_dict = {}
+    for val1 in stats.values():
+        for val2 in val1.values():
+            copies_dict[val1["title"]] = float(val1["copies"])
+    copies_dict = OrderedDict(sorted(copies_dict.items(), key=lambda t: t[1]))
+    return list(copies_dict.items())[-1][0]
 
 
 def sum_sold(file_name):
@@ -30,7 +37,12 @@ def get_selling_avg(file_name):
     return mean(copies_list)
 
 
-# def count_longest_title(file_name):
+def count_longest_title(file_name):
+    names_list = []
+    for game in stats.keys():
+        names_list.append(game)
+    longest = sorted(names_list, key=len)[-1]
+    return len(longest)
 
 
 # def get_date_avg(file_name):
