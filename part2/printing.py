09cf51a778c02edd3b38894d093596b62d5a34db
Fruzsina Csongor <csongor.fruzsina@gmail.com>
2017-06-29 22:37:08 +0200

    added the printing function for the 1st five functions of reports module

--- part2/printing.py
+++ part2/printing.py
@@ -1,2 +1,12 @@
+import reports
 
-# Printing functions
+
+def print_the_answers(file_name):
+    print(reports.get_most_played(file_name))
+    print(reports.sum_sold(file_name))
+    print(reports.get_selling_avg(file_name))
+    print(reports.count_longest_title(file_name))
+    print(reports.get_date_avg(file_name))
+
+
+print_the_answers("game_stat.txt")
