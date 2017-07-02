9d508222b34301498bbc99a2a79b0a9305f069ff
Fruzsina Csongor <csongor.fruzsina@gmail.com>
2017-07-02 14:39:08 +0200

    added 'count_grouped_by_genre' and 'get_date_ordered' functions to get printed

--- part2/printing.py
+++ part2/printing.py
@@ -8,6 +8,8 @@ def print_the_answers(file_name, title):
     print(reports.count_longest_title(file_name))
     print(reports.get_date_avg(file_name))
     print(reports.get_game(file_name, title))
+    print(reports.count_grouped_by_genre(file_name))
+    print(reports.get_date_ordered(file_name))
 
 
 print_the_answers("game_stat.txt", "Counter-Strike")
