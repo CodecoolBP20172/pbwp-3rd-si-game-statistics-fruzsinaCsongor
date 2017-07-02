4a29a7f80af908f9e904c8e4b736793617031c15
Fruzsina Csongor <csongor.fruzsina@gmail.com>
2017-07-02 12:47:35 +0200

    added 'get_game' function to get printed

--- part2/printing.py
+++ part2/printing.py
@@ -1,12 +1,13 @@
 import reports
 
 
-def print_the_answers(file_name):
+def print_the_answers(file_name, title):
     print(reports.get_most_played(file_name))
     print(reports.sum_sold(file_name))
     print(reports.get_selling_avg(file_name))
     print(reports.count_longest_title(file_name))
     print(reports.get_date_avg(file_name))
+    print(reports.get_game(file_name, title))
 
 
-print_the_answers("game_stat.txt")
+print_the_answers("game_stat.txt", "Counter-Strike")
